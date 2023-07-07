045bd68164bf88d3a04fd50a06dbd113eddea67f
Thiago <thiago.kaique@stayfilm.com.br>
2023-07-07 10:38:26 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import "./layout.css"
+
+const main = 
+<div class="container">
+    <div class="menu">
+    </div>
+    <div class="header">
+    </div>
+    <div class="main">
+    </div>
+    <div class="footer">
+    </div>
+    <div class="right">
+    </div>
+</div>
+
+main.$parent(document.body)
